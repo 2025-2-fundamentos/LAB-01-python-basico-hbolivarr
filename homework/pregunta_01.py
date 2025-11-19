e3a4a2b5f1723eefc01676e6cd4f90b2d6d73f90
hbolivarr <hbolivarr@unal.edu.co>
2025-11-19 09:02:20 -0500

    Update pregunta_01.py

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -5,7 +5,6 @@ solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
 
-
 def pregunta_01():
     """
     Retorne la suma de la segunda columna.
